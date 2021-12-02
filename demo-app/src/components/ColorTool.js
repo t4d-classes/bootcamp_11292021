3eb3369f0f22950533eb6e081c849cfd710c6152
Eric Greene <eric@t4d.io>
2021-12-02 12:41:07 -0500

    add exercise 15

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -1,4 +1,5 @@
-import { useSortedList, SORT_ASC, SORT_DESC } from '../hooks/useSortedList';
+
+import { useColorToolStore } from '../hooks/useColorToolStore';
 
 import { ToolHeader } from "./ToolHeader";
 import { ColorList } from './ColorList';
@@ -6,32 +7,15 @@ import { ColorForm } from './ColorForm';
 
 export const ColorTool = (props) => {
 
-  const [
-    sortedColors, addColor,,deleteColor,
-    sortCol, setSortCol, sortDir, setSortDir
-  ] = useSortedList([ ...props.colors ], 'id', SORT_ASC);
-
-
-  const sortColors = () => {
-
-    if (sortCol === 'name' && sortDir === SORT_DESC) {
-      setSortCol('id');
-      setSortDir(SORT_ASC);
-    } else if (sortCol === 'id') {
-      setSortCol('name');
-    } else {
-      setSortDir(SORT_DESC);
-    }
-
-  };
+  const store = useColorToolStore([...props.colors ]);
 
   return (
     <>
       <ToolHeader headerText="Color Tool" />
-      <button type="button" onClick={sortColors}>
-        Current Sort: {sortCol}-{sortDir}</button>
-      <ColorList colors={sortedColors} onDeleteColor={deleteColor} />
-      <ColorForm buttonText="Add Color" onSubmitColor={addColor} />
+      <button type="button" onClick={store.sortColors}>
+        Current Sort: {store.sortCol}-{store.sortDir}</button>
+      <ColorList colors={store.sortedColors} onDeleteColor={store.deleteColor} />
+      <ColorForm buttonText="Add Color" onSubmitColor={store.addColor} />
     </>
   );
 
